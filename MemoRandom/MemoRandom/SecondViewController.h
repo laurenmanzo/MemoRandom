df8e02fe8ced4ffbac476e66e9a80216526cb91f
laurenmanzo <laurenelizabethmanzo@gmail.com>
2014-10-20 16:21:34 +1100

    Committing new tabbed application project.

---
+++ MemoRandom/MemoRandom/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MemoRandom
+//
+//  Created by Lauren Manzo on 26/10/14.
+//  Copyright (c) 2014 Lauren Manzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ MemoRandom/MemoRandomTests/MemoRandomTests.m
@@ -0,0 +1,34 @@
+//
+//  MemoRandomTests.m
+//  MemoRandomTests
+//
+//  Created by Lauren Manzo on 26/10/14.
+//  Copyright (c) 2014 Lauren Manzo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MemoRandomTests : XCTestCase
+
+@end
+
+@implementation MemoRandomTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
